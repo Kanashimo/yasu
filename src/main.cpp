d64dc5a0fea29c8713d0e7e3691c9ea179ae08b7
Kanashimo <hello@kanashimo.ovh>
2026-09-13 12:07:30 +0200

    chore: remove commented code in main

--- src/main.cpp
+++ src/main.cpp
@@ -149,19 +149,6 @@ void show_debug()
 
 int main()
 {
-    // try {
-    //     ScreenCapture screenCapture;
-    //     std::vector<Monitor> monitors = screenCapture.monitors();
-    //     for (Monitor monitor : monitors)
-    //     {
-    //         printf("\nmonitor: %d\nx: %d\ny: %d\nw: %d\nh: %d\nc: %s\n\n", monitor.id, monitor.x, monitor.y, monitor.width, monitor.height, monitor.connector.c_str());
-    //     }
-    // }
-    // catch (std::runtime_error &e) {
-    //     std::cerr << e.what() << std::endl;
-    //     return -1;
-    // }
-
     ScreenCapture screenCapture;
     const std::vector<Monitor> &monitors = screenCapture.monitors();
 
